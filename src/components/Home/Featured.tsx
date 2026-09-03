0e3bf12fe4101b862b9c7f5ad295b4efe08682ea
Abdul Basith <159468770+basthep@users.noreply.github.com>
2026-09-03 20:57:00 +0530

    Update API URL to use environment variable

--- src/components/Home/Featured.tsx
+++ src/components/Home/Featured.tsx
@@ -27,7 +27,7 @@ export default function Featured() {
         setError("");
 
         const response = await fetch(
-          "http://localhost:5000/api/franchises"
+          `${import.meta.env.VITE_API_URL}/api/franchises`
         );
 
         if (!response.ok) {
